3d521d8a12840c70f5bc0994440376cf2da2872e
minaya-web <minaya@choixmedia.com>
2026-08-06 13:32:53 +0400

    fix:warning fixed

--- dashboard/src/layout/Header.tsx
+++ dashboard/src/layout/Header.tsx
@@ -1,13 +1,29 @@
-import { useEffect } from "react"
-import api from "../api/api"
-
-const Header = () => {
-  useEffect(()=>{
-    api.get("/")
-  },[])
+import { useEffect } from "react";
+import api from "../api/api";
+import { FaList } from "react-icons/fa";
+const Header = ({
+  showSidebar,
+  setShowSidebar,
+}: {
+  showSidebar: boolean;
+  setShowSidebar: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  // useEffect(() => {
+  //   api.get("/");
+  // }, []);
   return (
-    <div>Header</div>
-  )
-}
+    <div className="fixed top-0 left-0 w-full py-5 px-2 lg:px-7 z-40">
+      <div className="ml-0 lg:ml-[260px] rounded-md h-[64px] flex items-start justify-between bg-[#b1addf] px-5 transitian-all duration-300"></div>
+      <div
+        onClick={() => setShowSidebar(!showSidebar)}
+        className="w-[35px] flex lg:hidden"
+      >
+        <span>
+          <FaList />
+        </span>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
--- dashboard/src/layout/SideBar.tsx
+++ dashboard/src/layout/SideBar.tsx
@@ -9,7 +9,13 @@ interface INav {
   role: string;
   path: string;
 }
-const SideBar = () => {
+const SideBar = ({
+  showSidebar,
+  setShowSidebar,
+}: {
+  showSidebar: boolean;
+  setShowSidebar: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
   const [allNav, setAllNav] = useState<INav[]>([]);
   const { pathname } = useLocation();
   console.log("pathname:", pathname);
@@ -22,10 +28,13 @@ const SideBar = () => {
   console.log("allNav:", allNav);
   return (
     <div>
-      <div></div>
+      <div
+        onClick={() => setShowSidebar(false)}
+        className={`flex duration-200 ${showSidebar ? "invisible" : "visible"} w-screen h-screen bg-[#22292f80] top-0 left-0 z-10`}
+      ></div>
 
       <div
-        className={`w-[260px] fixed bg-[#e6e7fb] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all `}
+        className={`w-[260px] fixed bg-[#e6e7fb] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all ${showSidebar ? "left-0" : "-left-[260px] lg:left-0"} `}
       >
         <div className="h-[70px] flex justify-center items-center">
           <Link to="/" className="w-[180px] h-[50px]">
